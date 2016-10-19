a1f1a8caadba22b8cd26eaa3928b3c21db20753d
Christian Nieves <me@christiannieves.com>
2016-10-19 12:33:17 -0600

    Add template.cpp file to root

---
+++ template.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: Christian Nieves
+ * Assignment: XXXXXXXXXXXXXXX
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* INSERT YOUR CODE HERE :) */
+    return 0;
+}
